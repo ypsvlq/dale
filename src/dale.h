69208b389dfc316ae25760f1286ebd16fdecf51f
Elaine Gibson <ypsvlq@gmail.com>
2021-07-05 11:53:42 +0100

    Support building without stdnoreturn.h

--- src/dale.h
+++ src/dale.h
@@ -1,4 +1,8 @@
+#if __STDC_VERSION__ >= 201112L
 #include <stdnoreturn.h>
+#else
+#define noreturn
+#endif
 #include <stddef.h>
 #include <stdio.h>
 
--- src/util.c
+++ src/util.c
@@ -1,4 +1,3 @@
-#include <stdnoreturn.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
