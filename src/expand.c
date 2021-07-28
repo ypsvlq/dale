24baa5d6d0fbfb3d9099d425d0f6f2e1432043f5
Elaine Gibson <ypsvlq@gmail.com>
2021-07-28 12:10:31 +0100

    Add variable expansions that call builtins

--- src/expand.c
+++ src/expand.c
@@ -3,15 +3,45 @@
 #include <stdbool.h>
 #include "dale.h"
 
+static size_t bscan(const char *str, char ob, char cb) {
+	char search[] = {cb, '$', '\0'};
+	size_t sz = 0;
+	size_t depth = 1;
+	while (depth) {
+		sz += strcspn(str+sz, search);
+		if (!str[sz]) {
+			err("Unterminated expansion");
+		} else if (str[sz] == cb) {
+			depth--;
+			sz++;
+		} else if (str[sz+1] == '$') {
+			sz += 2;
+		} else if (str[sz+1] == ob) {
+			depth++;
+			sz += 2;
+		} else {
+			sz++;
+		}
+	}
+	return sz;
+}
+
 char *varexpand(const char *str) {
-	enum {NONE, NORMAL, COND} type;
-	static const char brackets[] = {0, '(', '['};
-	static const char cbrackets[] = {0, ')', ']'};
+	static const struct builtin {
+		char *name;
+		char *(*fn)(const char *str);
+	} builtins[] = {
+		{"find", hostfind},
+	};
+
+	enum {NONE, NORMAL, COND, BUILTIN} type;
+	static const char brackets[] = {0, '(', '[', '{'};
+	static const char cbrackets[] = {0, ')', ']', '}'};
 	char *bstack = NULL;
 	size_t nbstack = 0;
 
 	char *out, *p, *p2, *p3;
-	size_t len, sz, depth;
+	size_t len, sz;
 	bool negate;
 
 	out = NULL;
@@ -52,7 +82,7 @@ char *varexpand(const char *str) {
 				for (sz = 0; str[sz]; sz++) {
 					if (str[sz] == '$') {
 						sz++;
-						if (strchr("([", str[sz])) {
+						if (strchr("([{", str[sz])) {
 							bstack = xrealloc(bstack, ++nbstack);
 							for (size_t i = 0; i < LEN(brackets); i++) {
 								if (str[sz] == brackets[i]) {
@@ -73,54 +103,55 @@ char *varexpand(const char *str) {
 					err("Unmatched brackets");
 				p = xstrndup(str, sz);
 				p3 = varexpand(p);
-				p2 = varget(p3);
 				free(p);
-				free(p3);
+				if (type != BUILTIN) {
+					p2 = varget(p3);
+					free(p3);
+				}
 			}
 			str += sz;
-			if (type == COND) {
+			if (type == COND || type == BUILTIN) {
 				if (!*str || !*++str)
-					err("Unterminated conditional expansion");
-				sz = 0;
-				depth = 1;
-				while (depth) {
-					sz += strcspn(str+sz, "$]");
-					if (!str[sz]) {
-						err("Unterminated conditional expansion");
-					} else if (str[sz] == ']') {
-						depth--;
-						sz++;
-					} else if (str[sz+1] == '$') {
-						sz += 2;
-					} else if (str[sz+1] == '[') {
-						depth++;
-						sz += 2;
-					} else {
-						sz++;
-					}
-				}
+					err("Unterminated expansion");
+				sz = bscan(str, brackets[type], cbrackets[type]);
 				p = xstrndup(str, sz-1);
 				str += sz;
-				if ((!negate && p2 && *p2 == '1') || (negate && (!p2 || *p2 != '1'))) {
-					p2 = varexpand(p);
-					sz = strlen(p2);
-					out = xrealloc(out, len+sz);
-					memcpy(out+len, p2, sz);
-					len += sz;
-					free(p2);
+				if (type == COND) {
+					if ((!negate && p2 && *p2 == '1') || (negate && (!p2 || *p2 != '1'))) {
+						p2 = varexpand(p);
+						sz = strlen(p2);
+						out = xrealloc(out, len+sz);
+						memcpy(out+len, p2, sz);
+						len += sz;
+						free(p2);
+					}
+				} else if (type == BUILTIN) {
+					for (size_t i = 0; i < LEN(builtins); i++) {
+						if (!strcmp(builtins[i].name, p3)) {
+							p2 = builtins[i].fn(p);
+							if (p2) {
+								sz = strlen(p2);
+								out = xrealloc(out, len+sz);
+								memcpy(out+len, p2, sz);
+								len += sz;
+								free(p2);
+							}
+						}
+					}
+					free(p3);
 				}
 				free(p);
-				continue;
-			}
-			sz = strlen(p2);
-			if (sz) {
-				out = xrealloc(out, len+sz+1);
-				memcpy(out+len, p2, sz);
-				len += sz;
+			} else {
+				sz = strlen(p2);
+				if (sz) {
+					out = xrealloc(out, len+sz+1);
+					memcpy(out+len, p2, sz);
+					len += sz;
+				}
 			}
 			if (type == NORMAL) {
 				if (*str != ')')
-					err("Unterminated variable expansion");
+					err("Unterminated expansion");
 				str++;
 			}
 		}
