00a8c940626d1c141b9d253c1a027e1c88db134b
Elaine Gibson <ypsvlq@gmail.com>
2021-08-03 17:25:20 +0100

    Restructure main.c

--- src/main.c
+++ src/main.c
@@ -12,46 +12,13 @@ size_t ntasks;
 struct tc *tcs;
 size_t ntcs;
 
-static char *upperstr(const char *s) {
-	char *out = xmalloc(strlen(s) + 1);
-	for (size_t i = 0; s[i]; i++)
-		out[i] = toupper(s[i]);
-	out[strlen(s)] = 0;
-	return out;
-}
+static void optparse(int argc, char *argv[], char ***pwant, size_t *pnwant);
+static struct tc *loadtc(void);
 
-static void varpfxarr(const char *var, const char *pfx, char **arr, size_t len) {
-	char *out, *tmp;
-	if (!len)
-		return;
-	asprintf(&out, "%s%s ", pfx, arr[0]);
-	for (size_t i = 1; i < len; i++) {
-		asprintf(&tmp, "%s%s%s ", out, pfx, arr[i]);
-		free(out);
-		out = tmp;
-	}
-	varappend(var, out);
-	free(out);
-}
-
-static void taskvarset(const char *var, const char *name) {
-	char *p;
-	asprintf(&p, "$%s $%s_%s", var, name, var);
-	varsetp(var, varexpand(p));
-	free(p);
-}
-
-static void globarr(char ***parr, size_t *psz) {
-	char **arr = *parr;
-	size_t sz = *psz;
-	*parr = NULL;
-	*psz = 0;
-	for (size_t j = 0; j < sz; j++) {
-		glob(arr[j], parr, psz);
-		free(arr[j]);
-	}
-	free(arr);
-}
+static char *upperstr(const char *s);
+static void varpfxarr(const char *var, const char *pfx, char **arr, size_t len);
+static void taskvarset(const char *var, const char *name);
+static void globarr(char ***parr, size_t *psz);
 
 int main(int argc, char *argv[]) {
 	char *p, *p2, *p3, *p4;
@@ -62,116 +29,15 @@ int main(int argc, char *argv[]) {
 	size_t taskn = 1;
 	char **want = NULL;
 	size_t nwant = 0;
-	int pflag = 0;
-	char **lflag = NULL;
-	size_t nlflag = 0;
-	char **gflag = NULL;
-	size_t ngflag = 0;
-	bool Gflag = false;
-	char *bscript, *bdir, *tcname, *reqcflags, *reqlibs, *lang;
-	size_t jobs;
+	char *bscript, *bdir, *reqcflags, *reqlibs;
+	size_t jobs = 0;
 	uintmax_t um;
 	bool verbose;
 	char *exeext, *dllext;
 
 	hostinit();
 
-	for (int i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
-			if (strchr("lg", argv[i][1]) && !argv[i+1])
-				err("Option '-%c' requires an argument", argv[i][1]);
-			switch (argv[i][1]) {
-				case 'h':
-				case '?':
-					printf(
-						"usage: %s [options] [var=value]... [var+=value]... [task]...\n"
-						"\n"
-						"options:\n"
-						"  -h         Show this help\n"
-						"  -p         Process following var=value args after localscript(s)\n"
-						"  -l <file>  Load localscript from path, can be repeated to specify multiple\n"
-						"  -g <name>  Load named globalscript\n"
-						"  -G         Don't load global.dale\n"
-					, argv[0]);
-					return 0;
-				case 'p':
-					if (pflag)
-						fprintf(stderr, "Warning: '-p' option used multiple times\n");
-					else
-						pflag = i+1;
-					break;
-				case 'l':
-					lflag = xrealloc(lflag, sizeof(*lflag) * ++nlflag);
-					lflag[nlflag-1] = argv[++i];
-					break;
-				case 'g':
-					gflag = xrealloc(gflag, sizeof(*gflag) * ++ngflag);
-					gflag[ngflag-1] = argv[++i];
-					break;
-				case 'G':
-					Gflag = true;
-					break;
-				default:
-					err("Unknown option '%s'", argv[i]);
-			}
-		} else if ((p = strstr(argv[i], "+="))) {
-			if (!pflag) {
-				p2 = xstrndup(argv[i], p - argv[i]);
-				varappend(p2, p+2);
-			}
-		} else if (strchr(argv[i], '=')) {
-			if (!pflag) {
-				sz = strcspn(argv[i], "=");
-				p = xstrndup(argv[i], sz);
-				p2 = xstrdup(argv[i] + sz + 1);
-				varset(p, p2);
-			}
-		} else {
-			want = xrealloc(want, sizeof(*want) * ++nwant);
-			want[nwant-1] = argv[i];
-		}
-	}
-
-	if (!Gflag || gflag) {
-		arr = hostcfgs();
-		for (char **pdir = arr; *pdir; pdir++) {
-			if (!Gflag) {
-				asprintf(&p, "%s/global.dale", *pdir);
-				parsef(p, false);
-				free(p);
-			}
-			if (gflag && !*(pdir+1)) {
-				for (size_t i = 0; i < ngflag; i++) {
-					asprintf(&p, "%s/%s.dale", *pdir, gflag[i]);
-					parsef(p, true);
-					free(p);
-				}
-			}
-		}
-		free(gflag);
-	}
-
-	if (!lflag) {
-		parsef("local.dale", false);
-	} else {
-		for (size_t i = 0; i < nlflag; i++)
-			parsef(lflag[i], true);
-		free(lflag);
-	}
-
-	if (pflag) {
-		for (int i = pflag; i < argc; i++) {
-			if ((p = strstr(argv[i], "+="))) {
-				p2 = xstrndup(argv[i], p - argv[i]);
-				varappend(p2, p+2);
-			} else if (strchr(argv[i], '=')) {
-				sz = strcspn(argv[i], "=");
-				p = xstrndup(argv[i], sz);
-				p2 = xstrdup(argv[i] +  sz + 1);
-				varset(p, p2);
-			}
-		}
-	}
+	optparse(argc, argv, &want, &nwant);
 
 	if (*varget("_nodefvar") != '1')
 		hostsetvars();
@@ -182,83 +48,24 @@ int main(int argc, char *argv[]) {
 	bdir = vargetnull("_bdir");
 	if (!bdir)
 		bdir = "build";
-	tcname = vargetnull("_tcname");
 	verbose = vargetnull("_verbose");
 	reqcflags = vargetnull("_reqcflags");
 	reqlibs = vargetnull("_reqlibs");
 	p = vargetnull("_jobs");
+	exeext = varget("_exeext");
+	dllext = varget("_dllext");
 	if (p) {
 		errno = 0;
 		um = strtoumax(p, NULL, 10);
 		if (um > SIZE_MAX || errno == ERANGE)
 			err("Overflow");
 		jobs = um;
-	} else {
-		jobs = 0;
 	}
 
 	parsef(bscript, true);
-	lang = vargetnull("_lang");
-	if (!lang)
-		lang = "c";
 
-	for (size_t i = 0; i < ntcs; i++) {
-		if (tcname && strcmp(tcs[i].name, tcname))
-			continue;
-		if (!tcs[i].find || !tcs[i].objext || !tcs[i].libext || !tcs[i].libpfx || !tcs[i].incpfx || !tcs[i].defpfx || !tcs[i].compile || !tcs[i].linkexe || !tcs[i].linklib || !tcs[i].linkdll) {
-			fprintf(stderr, "Warning: Skipping underspecified toolchain '%s'\n", tcs[i].name);
-			continue;
-		}
-		if (strcmp(tcs[i].lang, lang))
-			continue;
-		for (size_t j = 0; j < tcs[i].nfind; j++) {
-			if ((p3 = strchr(tcs[i].find[j], '='))) {
-				p = xstrndup(tcs[i].find[j], p3 - tcs[i].find[j]);
-				puts(p);
-				p2 = upperstr(p);
-				free(p);
-			} else {
-				p3 = NULL;
-				p2 = upperstr(tcs[i].find[j]);
-			}
-			if (vargetnull(p2)) {
-				free(p2);
-				continue;
-			}
-			p = hostfind(p3 ? p3+1 : tcs[i].find[j]);
-			if (p) {
-				varset(p2, p);
-				if (j+1 == tcs[i].nfind)
-					tc = &tcs[i];
-			} else {
-				free(p2);
-				for (size_t k = j; k;) {
-					k--;
-					p = upperstr(tcs[i].find[k]);
-					varunset(p);
-					free(p);
-				}
-				break;
-			}
-		}
-		if (tc)
-			break;
-	}
-	if (!tc) {
-		if (!tcname)
-			fputs("Error: No valid toolchain found (tried:", stderr);
-		else
-			fprintf(stderr, "Error: Unknown toolchain '%s' (known:", tcname);
-		for (size_t i = 0; i < ntcs; i++)
-			if (!strcmp(tcs[i].lang, lang))
-				fprintf(stderr, " %s", tcs[i].name);
-		fputs(")\n", stderr);
-		return 1;
-	}
+	tc = loadtc();
 	printf("Using toolchain '%s'\n", tc->name);
-
-	exeext = varget("_exeext");
-	dllext = varget("_dllext");
 
 	for (size_t i = 0; i < nwant; i++) {
 		for (size_t j = 0; j < ntasks; j++) {
@@ -270,6 +77,7 @@ int main(int argc, char *argv[]) {
 		err("Unknown task '%s'", want[i]);
 wantfound:;
 	}
+
 
 	if (tasks) {
 		hostmkdir(bdir);
@@ -438,4 +246,229 @@ wantfound:;
 	}
 
 	hostquit();
+}
+
+static void optparse(int argc, char *argv[], char ***pwant, size_t *pnwant) {
+	char *p, *p2;
+	size_t sz;
+
+	char **want = NULL;
+	size_t nwant = 0;
+	int pflag = 0;
+	char **lflag = NULL;
+	size_t nlflag = 0;
+	char **gflag = NULL;
+	size_t ngflag = 0;
+	bool Gflag = false;
+
+	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (strchr("lg", argv[i][1]) && !argv[i+1])
+				err("Option '-%c' requires an argument", argv[i][1]);
+			switch (argv[i][1]) {
+				case 'h':
+				case '?':
+					printf(
+						"usage: %s [options] [var=value]... [var+=value]... [task]...\n"
+						"\n"
+						"options:\n"
+						"  -h         Show this help\n"
+						"  -p         Process following var=value args after localscript(s)\n"
+						"  -l <file>  Load localscript from path, can be repeated to specify multiple\n"
+						"  -g <name>  Load named globalscript\n"
+						"  -G         Don't load global.dale\n"
+					, argv[0]);
+					exit(0);
+				case 'p':
+					if (pflag)
+						fprintf(stderr, "Warning: '-p' option used multiple times\n");
+					else
+						pflag = i+1;
+					break;
+				case 'l':
+					lflag = xrealloc(lflag, sizeof(*lflag) * ++nlflag);
+					lflag[nlflag-1] = argv[++i];
+					break;
+				case 'g':
+					gflag = xrealloc(gflag, sizeof(*gflag) * ++ngflag);
+					gflag[ngflag-1] = argv[++i];
+					break;
+				case 'G':
+					Gflag = true;
+					break;
+				default:
+					err("Unknown option '%s'", argv[i]);
+			}
+		} else if ((p = strstr(argv[i], "+="))) {
+			if (!pflag) {
+				p2 = xstrndup(argv[i], p - argv[i]);
+				varappend(p2, p+2);
+			}
+		} else if (strchr(argv[i], '=')) {
+			if (!pflag) {
+				sz = strcspn(argv[i], "=");
+				p = xstrndup(argv[i], sz);
+				p2 = xstrdup(argv[i] + sz + 1);
+				varset(p, p2);
+			}
+		} else {
+			want = xrealloc(want, sizeof(*want) * ++nwant);
+			want[nwant-1] = argv[i];
+		}
+	}
+
+	if (!Gflag || gflag) {
+		for (char **pdir = hostcfgs(); *pdir; pdir++) {
+			if (!Gflag) {
+				asprintf(&p, "%s/global.dale", *pdir);
+				parsef(p, false);
+				free(p);
+			}
+			if (gflag && !*(pdir+1)) {
+				for (size_t i = 0; i < ngflag; i++) {
+					asprintf(&p, "%s/%s.dale", *pdir, gflag[i]);
+					parsef(p, true);
+					free(p);
+				}
+			}
+		}
+		free(gflag);
+	}
+
+	if (!lflag) {
+		parsef("local.dale", false);
+	} else {
+		for (size_t i = 0; i < nlflag; i++)
+			parsef(lflag[i], true);
+		free(lflag);
+	}
+
+	if (pflag) {
+		for (int i = pflag; i < argc; i++) {
+			if ((p = strstr(argv[i], "+="))) {
+				p2 = xstrndup(argv[i], p - argv[i]);
+				varappend(p2, p+2);
+			} else if (strchr(argv[i], '=')) {
+				sz = strcspn(argv[i], "=");
+				p = xstrndup(argv[i], sz);
+				p2 = xstrdup(argv[i] +  sz + 1);
+				varset(p, p2);
+			}
+		}
+	}
+
+	*pwant = want;
+	*pnwant = nwant;
+}
+
+static struct tc *loadtc(void) {
+	char *tcname, *lang;
+	struct tc *tc = NULL;
+	char *p, *p2, *p3;
+
+	lang = vargetnull("_lang");
+	if (!lang)
+		lang = "c";
+
+	tcname = vargetnull("_tcname");
+
+	for (size_t i = 0; i < ntcs; i++) {
+		if (tcname && strcmp(tcs[i].name, tcname))
+			continue;
+		if (!tcs[i].find || !tcs[i].objext || !tcs[i].libext || !tcs[i].libpfx || !tcs[i].incpfx || !tcs[i].defpfx || !tcs[i].compile || !tcs[i].linkexe || !tcs[i].linklib || !tcs[i].linkdll) {
+			fprintf(stderr, "Warning: Skipping underspecified toolchain '%s'\n", tcs[i].name);
+			continue;
+		}
+		if (strcmp(tcs[i].lang, lang))
+			continue;
+
+		for (size_t j = 0; j < tcs[i].nfind; j++) {
+			if ((p3 = strchr(tcs[i].find[j], '='))) {
+				p = xstrndup(tcs[i].find[j], p3 - tcs[i].find[j]);
+				puts(p);
+				p2 = upperstr(p);
+				free(p);
+			} else {
+				p3 = NULL;
+				p2 = upperstr(tcs[i].find[j]);
+			}
+			if (vargetnull(p2)) {
+				free(p2);
+				continue;
+			}
+			p = hostfind(p3 ? p3+1 : tcs[i].find[j]);
+			if (p) {
+				varset(p2, p);
+				if (j+1 == tcs[i].nfind)
+					tc = &tcs[i];
+			} else {
+				free(p2);
+				for (size_t k = j; k;) {
+					k--;
+					p = upperstr(tcs[i].find[k]);
+					varunset(p);
+					free(p);
+				}
+				break;
+			}
+		}
+
+		if (tc)
+			break;
+	}
+
+	if (!tc) {
+		if (!tcname)
+			fputs("Error: No valid toolchain found (tried:", stderr);
+		else
+			fprintf(stderr, "Error: Unknown toolchain '%s' (known:", tcname);
+		for (size_t i = 0; i < ntcs; i++)
+			if (!strcmp(tcs[i].lang, lang))
+				fprintf(stderr, " %s", tcs[i].name);
+		fputs(")\n", stderr);
+		exit(1);
+	}
+
+	return tc;
+}
+
+static char *upperstr(const char *s) {
+	char *out = xmalloc(strlen(s) + 1);
+	for (size_t i = 0; s[i]; i++)
+		out[i] = toupper(s[i]);
+	out[strlen(s)] = 0;
+	return out;
+}
+
+static void varpfxarr(const char *var, const char *pfx, char **arr, size_t len) {
+	char *out, *tmp;
+	if (!len)
+		return;
+	asprintf(&out, "%s%s ", pfx, arr[0]);
+	for (size_t i = 1; i < len; i++) {
+		asprintf(&tmp, "%s%s%s ", out, pfx, arr[i]);
+		free(out);
+		out = tmp;
+	}
+	varappend(var, out);
+	free(out);
+}
+
+static void taskvarset(const char *var, const char *name) {
+	char *p;
+	asprintf(&p, "$%s $%s_%s", var, name, var);
+	varsetp(var, varexpand(p));
+	free(p);
+}
+
+static void globarr(char ***parr, size_t *psz) {
+	char **arr = *parr;
+	size_t sz = *psz;
+	*parr = NULL;
+	*psz = 0;
+	for (size_t j = 0; j < sz; j++) {
+		glob(arr[j], parr, psz);
+		free(arr[j]);
+	}
+	free(arr);
 }
